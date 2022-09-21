b73e24979a519ceb9dc8a94d17ead080d85e4ed7
Alejandro153 <alejandro.mr153@gmail.com>
2022-09-21 00:40:53 -0500

    Tarea1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -125,6 +125,10 @@ function mayorQueCincuenta(num) {
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+
+  a=x%y
+
+  return a
   
 }
 
@@ -132,51 +136,71 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if(num%2===0){
+
+    return true
+
+  }
+
+  return false
+
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if(num%2 !== 0){
+
+    return true
+
+  }
+
+  return false
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  a = Math.pow(num,2);
+  return a
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+  a = Math.pow(num,3);
+  return a 
   
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  
+  a = Math.pow(num,exponent);
+  return a
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+  a = Math.round(num);
+  return a
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
+  a = Math.ceil(num);
+  return a
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+  a = Math.random()
+  return a
 }
 
 function esPositivo(numero) {
@@ -184,47 +208,60 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if (numero>0){
+    a="Es positivo";
+    return a
+  } else if(numero<0){
+    a="Es negativo";
+    return a
+  } else {return false};
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  a = str + "!"
+  return a
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  a = nombre + " "+ apellido;
+  return a;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  a = "Hola " + nombre +"!";
+  return a;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  a = alto*ancho
+  return a
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  a = lado*4
+  return a
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+  a = (base*altura)/2;
+  return a;
 }
 
 
@@ -232,7 +269,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  a = euro*1.2;
+  return a;
 }
 
 
@@ -242,7 +280,25 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  switch(letra){
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+    case "A":
+    case "E":
+    case "I":
+    case "O":
+    case "U":
+      a = "Es vocal";
+      return a;
+      break;
+  }
+
+      a="Dato incorrecto";
+      return a;
+
 }
 
 
